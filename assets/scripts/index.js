1ec807d19d84b1aa896f0a48f2979be13ae1da69
J-Weeks <weeksjasons@gmail.com>
2016-02-25 16:33:01 -0500

    first commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// use require without a reference to ensure a file is bundled
+require('./example');
+
+$(document).ready(function(){
+  getDogs();
+});
+
+
+let getDogs = function(){
+  $.ajax({
+    url: "http://localhost:3000/dogs",
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(dogs){
+    displayDogs(dogs);
+  });
+};
+
+let displayDogs = function(dogs){
+  let dogsTemplate = require('./dogs.handlebars');
+  // debugger;
+  $('.primary-content').append(dogsTemplate({
+    dogs
+  }));
+};
